44fa9f4c8de44a38aea72a5d2e31343d7920d4c2
Alassane NIANG <adniang75@gmail.com>
2022-01-19 22:22:10 +0100

    JUnit 5 Integration Tests Overview

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetSDJpaServiceIT.java
@@ -0,0 +1,31 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PetSDJpaServiceIT {
+
+    @BeforeEach
+    void setUp () {
+    }
+
+    @Test
+    void findAll () {
+    }
+
+    @Test
+    void findById () {
+    }
+
+    @Test
+    void save () {
+    }
+
+    @Test
+    void delete () {
+    }
+
+    @Test
+    void deleteById () {
+    }
+}
